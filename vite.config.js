04e61a9d2c45254a20c5a374d6748826d49db79b
MarkoMTP <marko.mtp8@gmail.com>
2025-12-04 21:51:22 +0100

    excluded node modules from tests

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
+    exclude: ["node_modules", "dist", "node_modules 2"],
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/tests/setup.js",
